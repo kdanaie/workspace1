03226113f15b9e8e7c84de4a5f508b66e75d9c21
”Sedal <kdanaie@hotmail.com>
2017-09-20 10:36:55 +0000

    Validation of username and email on User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,10 @@
 class User < ActiveRecord::Base
-    validates :username, presence: true, length: {minimum:3, maximun: 50}
-    validates :email, presence: true, length: {minimum:7, maximun: 100}
+    validates :username,  presence: true, 
+                          length: {minimum:3, maximun: 50}, 
+                          uniqueness: {case_sensitive: false}
+    VALID_EMAIL_REGEX  = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+    validates :email,     presence: true, 
+                          length: {minimum:7, maximun: 100}, 
+                          uniqueness: {case_sensitive: false},
+                          format: { with: VALID_EMAIL_REGEX}
 end
