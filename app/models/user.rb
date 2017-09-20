cae0f3824d69b0dfc98a52c576ba3f944d76d0b0
”Sedal <kdanaie@hotmail.com>
2017-09-20 12:34:12 +0000

    Add secure password to users and attributes

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,4 +10,5 @@ class User < ActiveRecord::Base
                           length: {minimum:7, maximun: 100}, 
                           uniqueness: {case_sensitive: false},
                           format: { with: VALID_EMAIL_REGEX}
+    has_secure_password
 end
