12da7560116a9f1101293c8b047dff71e29df33d
”Sedal <kdanaie@hotmail.com>
2017-09-22 09:55:33 +0000

    update all views with category

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -57,12 +57,7 @@ class ArticlesController < ApplicationController
         
     end
     
-    def require_same_user
-        if current_user != @article.user  and !current_user.admin?
-            flash[:danger] = "You can only edit or delete your own articles"
-            redirect_to root_path
-        end
-    end
+
     
     private
         def set_article
@@ -70,7 +65,13 @@ class ArticlesController < ApplicationController
         end
         
         def article_params
-            params.require(:article).permit(:title, :description)
+            params.require(:article).permit(:title, :description, category_ids: [])
         end
-    
+        
+        def require_same_user
+            if current_user != @article.user  and !current_user.admin?
+                flash[:danger] = "You can only edit or delete your own articles"
+                redirect_to root_path
+            end
+        end    
 end
